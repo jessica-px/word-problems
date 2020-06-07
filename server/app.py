783cf5e20446379462d58900a8303f591e4fbe42
Jessica Peck <jessypeck@gmail.com>
2020-06-07 09:09:38 -0700

    Setup endpoints for use with react-router

--- server/app.py
+++ server/app.py
@@ -5,10 +5,18 @@ from server.generators.years import generate_years
 app = Flask(__name__, static_folder="../client", template_folder="../client")
 
 # -------------------------------------------------------------------------- #
-#                             Frontend Endpoint                              #
+#                            Frontend Endpoints                              #
 # -------------------------------------------------------------------------- #
+'''
+    These endpoints serve bundle.js, at which points react-router takes over
+    routing and displays the correct components based on the url.
+'''
 
-@app.route("/")
+@app.route('/<path:path>', methods=['GET'])
+def any_root_path(path):
+    return render_template('index.html')
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
 
@@ -16,6 +24,10 @@ def index():
 # -------------------------------------------------------------------------- #
 #                                API Endpoints                               #
 # -------------------------------------------------------------------------- #
+'''
+    These API endpoints can be used to GET/POST information between the
+    front and back ends.
+'''
 
 @app.route('/api/years', methods=['GET'])
 def get_years():
