f51408ae8b62a6b6166d185c5cf02ce014e53a97
Jessica Peck <jessypeck@gmail.com>
2020-06-07 09:09:16 -0700

    Add routes to index.js

--- client/index.js
+++ client/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+
 
 const App = () => (
-    <p>Hello World</p>
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <p>Hello World. This is the main page.</p>
+      </Route>
+      <Route path="/page2">
+        <p>Hello World???? This is NOT the main page.</p>
+      </Route>
+    </Switch>
+  </Router>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'));
