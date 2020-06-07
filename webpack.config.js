e6f8d4d85a14cd1025698becd7ec8057e1cef887
Jessica Peck <jessypeck@gmail.com>
2020-06-06 23:02:02 -0700

    Setup frontend boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+   entry: './client/index.js',
+   output: {
+      path: path.join(__dirname, '/client'),
+      filename: 'bundle.js'
+   },
+   devServer: {
+      inline: true,
+      port: 8001
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: { presets: ["@babel/env"] }
+         }
+      ]
+   },
+//    plugins:[
+//       new HtmlWebpackPlugin({
+//          template: './index.html'
+//       })
+//    ]
+}
