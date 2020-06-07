c77f339ca2433f11a00a58c85676a6e25be18677
Jessica Peck <jessypeck@gmail.com>
2020-06-06 19:50:43 -0700

    Add endpoint for get_years()

--- server/app.py
+++ server/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from io import BytesIO
+from flask import Flask, jsonify, make_response, send_file
+from server.generators.years import generate_years
 
 app = Flask(__name__)
+
+# -------------------------------------------------------------------------- #
+#                             Frontend Endpoint                              #
+# -------------------------------------------------------------------------- #
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+
+# -------------------------------------------------------------------------- #
+#                                API Endpoints                               #
+# -------------------------------------------------------------------------- #
+
+@app.route('/api/years', methods=['GET'])
+def get_years():
+    pdf_byte_string = generate_years()
+    return jsonify(pdf_byte_string)
 
 if __name__ == '__main__':
     app.run(debug=True)
