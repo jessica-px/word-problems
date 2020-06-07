f48cf1837f1fda19e15356ee6f9d50bee1925b44
Jessica Peck <jessypeck@gmail.com>
2020-06-07 09:34:54 -0700

    Import pages

--- client/index.js
+++ client/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { HomePage } from 'pages/homePage/HomePage.tsx';
+import { YearsPage } from 'pages/yearsPage/YearsPage.tsx';
 
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/">
-        <p>Hello World. This is the main page.</p>
+        <HomePage />
       </Route>
-      <Route path="/page2">
-        <p>Hello World???? This is NOT the main page.</p>
+      <Route path="/years">
+        <YearsPage />
       </Route>
     </Switch>
   </Router>
