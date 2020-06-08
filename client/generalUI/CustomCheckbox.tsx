bf9ba2045b1fee576a1d1f976e738d93b9a76c0a
Jessica Peck <jessypeck@gmail.com>
2020-06-07 17:51:37 -0700

    Add CustomCheckbox component

---
+++ client/generalUI/CustomCheckbox.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+interface CustomCheckboxProps {
+  label: string,
+  defaultState: boolean
+}
+
+export const CustomCheckbox = ({label, defaultState}: CustomCheckboxProps) => {
+  const [checked, setChecked] = React.useState(defaultState);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  return (
+    <div>
+      <FormControlLabel
+        control={<Checkbox checked={checked} onChange={handleChange} name={label} />}
+        label={label}
+      />
+    </div>
+  );
+}
