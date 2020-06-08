a1558a672ca82249c2b070a5ba5eac20435aad30
Jessica Peck <jessypeck@gmail.com>
2020-06-07 22:00:19 -0700

    Move <PageInfo /> to own file

---
+++ client/generalUI/PageInfo.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import styled from 'styled-components'
+import { withStyles, Theme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Chip from '@material-ui/core/Chip';
+import LaunchIcon from '@material-ui/icons/Launch';
+import Tooltip from '@material-ui/core/Tooltip';
+
+import { PALETTE } from 'generalUI/styleConstants';
+
+
+// -------------------------------------------------------------------------- //
+//                                Main Component                              //
+// -------------------------------------------------------------------------- //
+/*
+    <PageInfo /> renders the upper "info" portfion of a typical generator page,
+    including the title, description, examples, etc.
+*/
+
+interface PageInfoProps {
+  pageTitle: string,
+  standard: {
+    name: string,
+    description: string,
+    url: string
+  },
+  chipLabels: string[],
+  exampleSentences: string[],
+  descriptionBody: React.ReactNode;
+}
+
+
+export const PageInfo = (props: PageInfoProps) => (
+  <React.Fragment>
+    <PageTitle>
+      <h1>{props.pageTitle}</h1>
+      <StandardsLink
+        name={props.standard.name}
+        description={props.standard.description}
+        url={props.standard.url}
+      />
+    </PageTitle>
+    <Chips chipLabels={props.chipLabels}/>
+    <h2>Description</h2>
+    {props.descriptionBody}
+    <h3>Examples</h3>
+    <ExampleCard exampleSentences={props.exampleSentences}/>
+  </React.Fragment>
+)
+
+// -------------------------------------------------------------------------- //
+//                              Sub-Components                                //
+// -------------------------------------------------------------------------- //
+
+interface StandardsLinkProps {
+  name: string,
+  description: string,
+  url: string
+}
+
+const StandardsLink = ({name, description, url}: StandardsLinkProps) => (
+  <LightTooltip title={description} >
+    <a href={url}>
+        {name} <LaunchIcon fontSize="small"/>
+    </a>
+  </LightTooltip>
+)
+
+interface ChipsProps {
+  chipLabels: string[]
+}
+
+const Chips = ({chipLabels}: ChipsProps) => (
+  <ChipContainer>
+      {chipLabels.map(label => <Chip label={label} variant="outlined" key={label} />)}
+  </ChipContainer>
+)
+
+interface ExampleCardProps {
+  exampleSentences: string[]
+}
+
+const ExampleCard = ({exampleSentences}: ExampleCardProps) => (
+  <Card variant="outlined">
+    <CardContent>
+      {exampleSentences.map((sentence, i) => <p key={i}>{sentence}</p>)}
+    </CardContent>
+  </Card>
+)
+
+// -------------------------------------------------------------------------- //
+//                                  Styles                                    //
+// -------------------------------------------------------------------------- //
+
+const PageTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const ChipContainer = styled.div`
+  & > * {
+    margin-right: 15px;
+  }
+`
+
+const LightTooltip = withStyles((theme: Theme) => ({
+  tooltip: {
+    backgroundColor: PALETTE.background_light,
+    color: 'rgba(0, 0, 0, 0.87)',
+    boxShadow: theme.shadows[1],
+    fontSize: 12,
+    fontWeight: 400,
+    lineHeight: 2
+  },
+}))(Tooltip);
--- client/pages/yearsPage/YearsPage.tsx
+++ client/pages/yearsPage/YearsPage.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components'
-import { withStyles, Theme } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Chip from '@material-ui/core/Chip';
-import LaunchIcon from '@material-ui/icons/Launch';
-import Tooltip from '@material-ui/core/Tooltip';
+import styled from 'styled-components'
 import Button from '@material-ui/core/Button';
 import GetAppIcon from '@material-ui/icons/GetApp';
 import OpenInNewIcon from '@material-ui/icons/OpenInNew';
@@ -13,7 +7,7 @@ import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import { CustomSlider } from 'generalUI/CustomSlider';
 import { CustomCheckbox } from 'generalUI/CustomCheckbox';
 import { RadioForm } from 'generalUI/RadioForm';
-import { PALETTE } from 'generalUI/styleConstants';
+import { PageInfo } from 'generalUI/PageInfo';
 
 // -------------------------------------------------------------------------- //
 //                                Page Setup                                  //
@@ -37,9 +31,11 @@ const exampleSentences = [
   "3. Michael was born in 1996. Emmet is 55 years older than Michael. In what year was Emmet born?"
 ];
 
-const standardName = 'CCSS.Math.Content.2.OA.A.1'
-const standardDescription = 'Use addition and subtraction within 100 to solve one- and two-step word problems involving situations of adding to, taking from, putting together, taking apart, and comparing, with unknowns in all positions, e.g., by using drawings and equations with a symbol for the unknown number to represent the problem.'
-const standardUrl = 'http://www.corestandards.org/Math/Content/2/OA/'
+const standard = {
+  name: 'CCSS.Math.Content.2.OA.A.1',
+  description: 'Use addition and subtraction within 100 to solve one- and two-step word problems involving situations of adding to, taking from, putting together, taking apart, and comparing, with unknowns in all positions, e.g., by using drawings and equations with a symbol for the unknown number to represent the problem.',
+  url: 'http://www.corestandards.org/Math/Content/2/OA/'
+}
 
 // -------------------------------------------------------------------------- //
 //                              Main Component                                //
@@ -47,7 +43,13 @@ const standardUrl = 'http://www.corestandards.org/Math/Content/2/OA/'
 
 export const YearsPage = () => (
   <React.Fragment>
-    <PageInfo />
+    <PageInfo
+      pageTitle={pageTitle}
+      chipLabels={chipLabels}
+      exampleSentences={exampleSentences}
+      standard={standard}
+      descriptionBody={<DescriptionBody />}
+    />
     <ConfigurationSection />
   </React.Fragment>
 )
@@ -111,6 +113,10 @@ const ConfigurationSection = () => (
   </React.Fragment>
 )
 
+// -------------------------------------------------------------------------- //
+//                                 Styles                                     //
+// -------------------------------------------------------------------------- //
+
 const ButtonWrapper = styled.div`
   display: flex;
   justify-content: space-around;
@@ -130,81 +136,3 @@ const Column = styled.span`
     margin-bottom: 50px;
   }
 `
-
-
-// -------------------------------------------------------------------------- //
-//                              Sub-Components                                //
-// -------------------------------------------------------------------------- //
-
-const PageInfo = () => (
-  <React.Fragment>
-    <PageTitle>
-      <h1>{pageTitle}</h1>
-      <StandardsLink
-        name={standardName}
-        description={standardDescription}
-        url={standardUrl}
-      />
-    </PageTitle>
-    <Chips />
-    <h2>Description</h2>
-    <DescriptionBody />
-    <h3>Examples</h3>
-    <ExampleCard />
-  </React.Fragment>
-)
-
-interface StandardsLinkProps {
-  name: string,
-  description: string,
-  url: string
-}
-
-const StandardsLink = ({name, description, url}: StandardsLinkProps) => (
-  <LightTooltip title={description} >
-    <a href={url}>
-        {name} <LaunchIcon fontSize="small"/>
-    </a>
-  </LightTooltip>
-)
-
-const Chips = () => (
-  <ChipContainer>
-      {chipLabels.map(label => <Chip label={label} variant="outlined" key={label} />)}
-  </ChipContainer>
-)
-
-const ExampleCard = () => (
-  <Card variant="outlined">
-    <CardContent>
-      {exampleSentences.map((sentence, i) => <p key={i}>{sentence}</p>)}
-    </CardContent>
-  </Card>
-)
-
-// -------------------------------------------------------------------------- //
-//                                  Styles                                    //
-// -------------------------------------------------------------------------- //
-
-const PageTitle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
-
-const ChipContainer = styled.div`
-  & > * {
-    margin-right: 15px;
-  }
-`
-
-const LightTooltip = withStyles((theme: Theme) => ({
-  tooltip: {
-    backgroundColor: PALETTE.background_light,
-    color: 'rgba(0, 0, 0, 0.87)',
-    boxShadow: theme.shadows[1],
-    fontSize: 12,
-    fontWeight: 400,
-    lineHeight: 2
-  },
-}))(Tooltip);
