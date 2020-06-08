645826f5771d235a1134b9658634c23b73585235
Jessica Peck <jessypeck@gmail.com>
2020-06-07 17:51:52 -0700

    Add RadioForm component

---
+++ client/generalUI/RadioForm.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+
+
+interface RadioFormProps {
+  label: string,
+  options: any
+}
+
+export const RadioForm = ({label, options}: RadioFormProps) => {
+  const [value, setValue] = React.useState(options[0]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue((event.target as HTMLInputElement).value);
+  };
+
+  return (
+    <div>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">{label}</FormLabel>
+        <RadioGroup aria-label={label} name={label} value={value} onChange={handleChange}>
+          {options.map((option: string) => (
+            <FormControlLabel value={option} control={<Radio />} label={option} />
+          ))}
+        </RadioGroup>
+      </FormControl>
+    </div>
+  );
+}
