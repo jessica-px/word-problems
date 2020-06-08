365eb00eeb4a3b395bc39e455718f7e0efde7bf1
Jessica Peck <jessypeck@gmail.com>
2020-06-07 21:37:21 -0700

    Add style constants

---
+++ client/generalUI/styleConstants.ts
@@ -0,0 +1,15 @@
+
+export const COLORS = {
+    dark_green: '#03363d',
+    greensea: '#16a085',
+    white: '#fefefe',
+    mint: '#d1e8df',
+    faded_mint: '#b3c2c4'
+}
+
+
+export const PALETTE = {
+    primary:  COLORS.dark_green,
+    background_light: COLORS.white,
+    background_medium: COLORS.mint
+}
